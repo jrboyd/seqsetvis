1bf22bf9ad5401c515974b3a7621c589ef6312ee
jrboyd <jrboyd@med.uvm.edu>
2017-12-24 16:21:36 -0500

    uncomment testthat.R

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,8 +1,8 @@
-#
-# ## load dependencies
-# library(testthat)
-# library(seqsetvis)
-# library(data.table)
-#
-# ## test package
-# test_check(package = "seqsetvis", path = "seqsetvis.Rcheck/00_pkg_src/seqsetvis/tests/testthat/", filter = "test_")
+
+## load dependencies
+library(testthat)
+library(seqsetvis)
+library(data.table)
+
+## test package
+test_check(package = "seqsetvis", path = "seqsetvis.Rcheck/00_pkg_src/seqsetvis/tests/testthat/", filter = "test_")
