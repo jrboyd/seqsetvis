c93e7a3a4bbb21f682e133d54616e253775b170b
jrboyd <jrboyd@med.uvm.edu>
2017-12-24 16:28:43 -0500

    testthat again

--- tests/testthat.R
+++ tests/testthat.R
@@ -5,4 +5,4 @@ library(seqsetvis)
 library(data.table)
 
 ## test package
-test_check(package = "seqsetvis", path = "seqsetvis.Rcheck/00_pkg_src/seqsetvis/tests/testthat/", filter = "test_")
+test_check(package = "seqsetvis")
