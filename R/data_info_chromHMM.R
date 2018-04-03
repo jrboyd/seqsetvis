ddefb846f22e3ddedebb0ed8d081df73f9e7f912
jrboyd <jrboyd@med.uvm.edu>
2018-04-03 19:48:04 -0400

    v bump 0.99.8 clean build local

--- R/data_info_chromHMM.R
+++ R/data_info_chromHMM.R
@@ -16,6 +16,7 @@
 #' @details Contains: \itemize{
 #' \item \code{\link{chromHMM_demo_overlaps_gr}}
 #' \item \code{\link{chromHMM_demo_bw_states_gr}}
+#' \item \code{\link{chromHMM_demo_state_total_widths}}
 #' \item \code{\link{chromHMM_demo_state_colors}}
 #' \item \code{\link{chromHMM_demo_segmentation_url}}
 #' \item \code{\link{chromHMM_demo_chain_url}}
@@ -27,7 +28,7 @@ NULL
 
 
 
-#' URL to download MCF7 chromHMM segmentation
+#' URL to download hg19 MCF7 chromHMM segmentation
 #'
 #' @details file is gzipped bed with name, score, itemRgb and thick meta columns
 #' @details part of \code{\link{chromHMM_demo_data}}
@@ -49,7 +50,17 @@ NULL
 #' @format a character containing a URL
 NULL
 
-#' original state color mappings stored in segmentation bed
+#' state name to total width mappings, hg38
+#'
+#' @details part of \code{\link{chromHMM_demo_data}}
+#'
+#' @docType data
+#' @keywords datasets
+#' @name chromHMM_demo_state_total_widths
+#' @format named numeric of total widths per state
+NULL
+
+#' original state name to color mappings stored in segmentation bed
 #'
 #' @details part of \code{\link{chromHMM_demo_data}}
 #'
@@ -59,7 +70,7 @@ NULL
 #' @format a named character vector mapping states to hex colors
 NULL
 
-#' overlap of MCF10A CTCF with MCF7 chromHMM states
+#' overlap of MCF10A CTCF with MCF7 chromHMM states, hg38.
 #'
 #' @details part of \code{\link{chromHMM_demo_data}}
 #' @details the result of ssvOverlapIntervalSets() on MCF10A CTCF peaks and
@@ -74,7 +85,7 @@ NULL
 #' with 10 logical metadata columns, 1 per state.
 NULL
 
-#' MCF10A CTCF profiles at 20 windows per chromHMM state.
+#' MCF10A CTCF profiles at 20 windows per chromHMM state, hg38.
 #'
 #' @details part of \code{\link{chromHMM_demo_data}}
 #' @details the result of fetchWindowedBigwig() on the MCF10A_CTCF_FE.bw
