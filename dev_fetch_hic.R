7a5b6a658d19c1a616ed2e3d6f63c308b8df5afd
jrboyd <jrboyd@med.uvm.edu>
2018-04-09 11:29:41 -0400

    begin hic module

---
+++ dev_fetch_hic.R
@@ -0,0 +1,12 @@
+#2 types of hic fetch
+#  one to many bigwig
+#  tad score
+library(dthic)
+library(seqsetvis)
+
+r1 = "~/HiC-Pro/outputs/MCF10A/hic_results/matrix/rep1"
+r2 = "~/HiC-Pro/outputs/MCF10A/hic_results/matrix/rep2
+"
+
+HiC_matrix_wInsulation(matrix_file = "~/HiC-Pro/outputs/MCF10A/hic_results/matrix/rep1/iced/40000/rep1_40000_iced.matrix",
+                       regions_file = "~/HiC-Pro/outputs/MCF10A/hic_results/matrix/rep1/, parameters = )
