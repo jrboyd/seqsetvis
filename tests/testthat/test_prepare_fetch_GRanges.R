928f6efea41846d78e4ca26a6d028eb4a5de3b69
jrboyd <jrboyd@med.uvm.edu>
2018-08-15 18:41:11 -0400

    bugfix: prepare_fetch_GRanges incorrectly was returning ranges starting <= 0. now offending ranges are shifted to correct.

---
+++ tests/testthat/test_prepare_fetch_GRanges.R
@@ -0,0 +1,21 @@
+# flipping viewGranges
+library(seqsetvis)
+library(GenomicRanges)
+library(testthat)
+library(data.table)
+
+qgr = CTCF_in_10a_overlaps_gr[1:5]
+start(qgr) = seq_len(5)
+end(qgr) = 3*seq_len(5)
+qgr = GenomicRanges::shift(qgr, -2)
+strand(qgr) = c("+", "-", "-", "+", "-")
+
+#sampling
+test_that("prepare_fetch_GRanges start never less than 1", {
+    expect_warning({
+        tgr = prepare_fetch_GRanges(qgr, win_size = 5, target_size = 10)
+    }, regexp = "Some out of bounds")
+    expect_true(all(width(tgr) == 10))
+    expect_true(all(start(tgr) >= 1))
+})
+
