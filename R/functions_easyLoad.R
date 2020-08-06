0782ee03c1c891cb61f4e0868eb209396509803e
jrboyd <jrboyd@med.uvm.edu>
2020-08-06 14:35:19 -0400

    added easyLoad_seacr

--- R/functions_easyLoad.R
+++ R/functions_easyLoad.R
@@ -1,5 +1,50 @@
 #functions that load a list of files as a list of GRanges
 
+#' easyLoad_bed takes a character vector of file paths to bed plus files and
+#' returning named list of GRanges.
+#' Mainly a utility function for loading MACS2 narrowPeak and broadPeak.
+#' @param file_paths character vector of paths to narrowPeak files.  If named,
+#' those names will be used in output unless overriden by providing file_names.
+#' @param file_names character vector of names for output list.  If not NULL
+#' will override any existing names for file_paths.  Default is NULL.
+#' @param extraCols named character vector of classes.  passed to
+#' rtracklayer::import for format = "BED". default is character().
+#' @return a named list of GRanges loaded from file_paths
+#' @import rtracklayer
+#' @export
+#' @examples
+#' bed_f = system.file("extdata/test_loading.bed",
+#'     package = "seqsetvis", mustWork = TRUE)
+#' easyLoad_bed(bed_f, "my_bed")
+easyLoad_bed = function(file_paths,
+                        file_names = NULL,
+                        extraCols = character()) {
+    if (is.factor(file_paths)) {
+        file_paths = as.character(file_paths)
+    }
+    stopifnot(is.character(file_paths))
+    stopifnot(all(file.exists(file_paths) |
+                      grepl("://", file_paths)))
+    #check names of file-paths
+    if (is.null(names(file_paths))) {
+        names(file_paths) = basename(file_paths)
+    }
+    #override names with file_names if not NULL
+    if (!is.null(file_names)) {
+        if (is.factor(file_names)) {
+            file_names = as.character(file_names)
+        }
+        stopifnot(is.character(file_paths))
+        stopifnot(length(file_paths) == length(file_names))
+        names(file_paths) = file_names
+    }
+    grs <- lapply(file_paths, function(f) {
+        rtracklayer::import(f, format = "BED",
+                            extraCols = extraCols)
+    })
+    grs
+}
+
 #' easyLoad_narrowPeak takes a character vector of file paths to narrowPeak
 #' files from MACS2 and returns a named list of GRanges.
 #' @param file_paths character vector of paths to narrowPeak files.  If named,
@@ -51,47 +96,25 @@ easyLoad_broadPeak = function(file_paths, file_names = NULL) {
                  extraCols = extraCols_broadPeak)
 }
 
-#' easyLoad_bed takes a character vector of file paths to bed plus files and
-#' returning named list of GRanges.
-#' Mainly a utility function for loading MACS2 narrowPeak and broadPeak.
-#' @param file_paths character vector of paths to narrowPeak files.  If named,
+#' easyLoad_seacr takes a character vector of file paths to seacr output bed files and returns a named list of GRanges.
+#' @param file_paths character vector of paths to seacr bed files.  If named,
 #' those names will be used in output unless overriden by providing file_names.
 #' @param file_names character vector of names for output list.  If not NULL
 #' will override any existing names for file_paths.  Default is NULL.
-#' @param extraCols named character vector of classes.  passed to
-#' rtracklayer::import for format = "BED". default is character().
 #' @return a named list of GRanges loaded from file_paths
 #' @import rtracklayer
 #' @export
 #' @examples
-#' bed_f = system.file("extdata/test_loading.bed",
+#' bed_f = system.file("extdata/test_loading.seacr.bed",
 #'     package = "seqsetvis", mustWork = TRUE)
-#' easyLoad_bed(bed_f, "my_bed")
-easyLoad_bed = function(file_paths,
-                        file_names = NULL,
-                        extraCols = character()) {
-    if (is.factor(file_paths)) {
-        file_paths = as.character(file_paths)
-    }
-    stopifnot(is.character(file_paths))
-    stopifnot(all(file.exists(file_paths) |
-                      grepl("://", file_paths)))
-    #check names of file-paths
-    if (is.null(names(file_paths))) {
-        names(file_paths) = basename(file_paths)
-    }
-    #override names with file_names if not NULL
-    if (!is.null(file_names)) {
-        if (is.factor(file_names)) {
-            file_names = as.character(file_names)
-        }
-        stopifnot(is.character(file_paths))
-        stopifnot(length(file_paths) == length(file_names))
-        names(file_paths) = file_names
-    }
-    grs <- lapply(file_paths, function(f) {
-        rtracklayer::import(f, format = "BED",
-                            extraCols = extraCols)
-    })
-    grs
+#' easyLoad_seacr(bed_f, "my_seacr")
+easyLoad_seacr = function(file_paths, file_names = NULL){
+    extraCols_seacr = c(
+        total_signal = "numeric",
+        max_signal = "numeric",
+        max_signal_region = "character"
+    )
+    easyLoad_bed(file_paths = file_paths,
+                 file_names = file_names,
+                 extraCols = extraCols_seacr)
 }
