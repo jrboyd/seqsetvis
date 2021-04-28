ede6f6fe7ecd55d68d56ccc29fdc5b62bb1eabf8
jrboyd <jrboyd@med.uvm.edu>
2021-04-28 15:09:38 -0400

    added easyLoad_FUN and easyLoad_IDRmerged

--- R/functions_easyLoad.R
+++ R/functions_easyLoad.R
@@ -1,27 +1,24 @@
-#functions that load a list of files as a list of GRanges
-
-#' easyLoad_bed takes a character vector of file paths to bed plus files and
-#' returning named list of GRanges.
-#' Mainly a utility function for loading MACS2 narrowPeak and broadPeak.
+#' easyLoad_FUN takes a character vector of file paths run an arbitrary function defined in load_FUN
 #'
 #' @param file_paths character vector of paths to narrowPeak files.  If named,
 #' those names will be used in output unless overriden by providing file_names.
+#' @param load_FUN Arbitrary function that takes at least a file path as argument.  May take other arguments that should be set in call to easyLoad_FUN.
 #' @param file_names character vector of names for output list.  If not NULL
 #' will override any existing names for file_paths.  Default is NULL.
-#' @param extraCols named character vector of classes.  passed to
-#' rtracklayer::import for format = "BED". default is character().
 #' @param n_cores number of cores to use, uses mc.cores option if set or 1.
-#' @return a named list of GRanges loaded from file_paths
-#' @import rtracklayer
+#' @param ... extra parameters passed to load_FUN
+#'
+#' @return a named list of results from load_FUN
 #' @export
+#'
 #' @examples
 #' bed_f = system.file("extdata/test_loading.bed",
 #'     package = "seqsetvis", mustWork = TRUE)
 #' easyLoad_bed(bed_f, "my_bed")
-easyLoad_bed = function(file_paths,
+easyLoad_FUN = function(file_paths, load_FUN,
                         file_names = NULL,
-                        extraCols = character(),
-                        n_cores = getOption("mc.cores", 1)) {
+                        n_cores = getOption("mc.cores", 1),
+                        ...){
     if (is.factor(file_paths)) {
         file_paths = as.character(file_paths)
     }
@@ -42,18 +39,79 @@ easyLoad_bed = function(file_paths,
         names(file_paths) = file_names
     }
     if(n_cores == 1){
-        grs <- lapply(file_paths, function(f) {
-            rtracklayer::import(f, format = "BED",
-                                extraCols = extraCols)
-        })
+        grs <- lapply(
+            file_paths, load_FUN, ...)
     }else{
-        grs = pbmcapply::pbmclapply(file_paths, function(f) {
-            rtracklayer::import(f, format = "BED",
-                                extraCols = extraCols)
-        },
-        mc.cores = n_cores)
+        grs = pbmcapply::pbmclapply(
+            file_paths,
+            load_FUN,
+            mc.cores = n_cores, ...)
     }
     grs
+}
+
+#' easyLoad_IDRmerged loads "overlapped-peaks.txt" from IDR.
+#'
+#' @param file_paths character vector of paths to narrowPeak files.  If named,
+#' those names will be used in output unless overriden by providing file_names.
+#' @param file_names character vector of names for output list.  If not NULL
+#' will override any existing names for file_paths.  Default is NULL.
+#' @param n_cores number of cores to use, uses mc.cores option if set or 1.
+#' @param max_idr maximum IDR value allowed
+#'
+#' @return named list of GRanges
+#' @export
+#'
+#' @examples
+#' idr_file = system.file("extdata/test_idr.overlapped-peaks.txt",
+#'     package = "seqsetvis", mustWork = TRUE)
+#' easyLoad_IDRmerged(idr_file)
+#' easyLoad_IDRmerged(idr_file, max_idr = .01)
+easyLoad_IDRmerged = function(file_paths,
+                              file_names = NULL,
+                              n_cores = getOption("mc.cores", 1),
+                              max_idr = .05){
+    load_idr = function(f, max_idr = .05){
+        idr_dt = suppressWarnings({data.table::fread(f)[IDR <= max_idr]})
+        GenomicRanges::GRanges(idr_dt[, .(seqnames = chr1, start = pmin(start1, start2), end = pmax(stop1, stop2), sig.value1, sig.value2, idr.local, IDR)])
+    }
+    easyLoad_FUN(file_paths, load_idr, file_names, n_cores, max_idr = max_idr)
+}
+
+#functions that load a list of files as a list of GRanges
+
+#' easyLoad_bed takes a character vector of file paths to bed plus files and
+#' returning named list of GRanges.
+#'
+#' Mainly a utility function for loading MACS2 narrowPeak and broadPeak.
+#'
+#' @param file_paths character vector of paths to narrowPeak files.  If named,
+#' those names will be used in output unless overriden by providing file_names.
+#' @param file_names character vector of names for output list.  If not NULL
+#' will override any existing names for file_paths.  Default is NULL.
+#' @param extraCols named character vector of classes.  passed to
+#' rtracklayer::import for format = "BED". default is character().
+#' @param n_cores number of cores to use, uses mc.cores option if set or 1.
+#' @return a named list of GRanges loaded from file_paths
+#' @import rtracklayer
+#' @export
+#' @examples
+#' bed_f = system.file("extdata/test_loading.bed",
+#'     package = "seqsetvis", mustWork = TRUE)
+#' easyLoad_bed(bed_f, "my_bed")
+easyLoad_bed = function(file_paths,
+                        file_names = NULL,
+                        extraCols = character(),
+                        n_cores = getOption("mc.cores", 1)) {
+    load_bed = function(f){
+        rtracklayer::import(f, format = "BED",
+                            extraCols = extraCols)
+    }
+
+    easyLoad_FUN(file_paths,
+                 load_bed,
+                 file_names = file_names,
+                 n_cores = n_cores)
 }
 
 #' easyLoad_narrowPeak takes a character vector of file paths to narrowPeak
